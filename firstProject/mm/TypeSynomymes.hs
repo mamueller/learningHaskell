ab25b86af97e8188bcbfde56a1757c9f70291ed5
Markus Müller <markus.mueller.1.g@googlemail.com>
2018-01-31 02:29:55 +0100

    telephonebook

---
+++ firstProject/mm/TypeSynomymes.hs
@@ -0,0 +1,15 @@
+type PhoneNumber = String
+type Name = String
+type PhoneBook =[(Name,PhoneNumber)]
+phoneBook :: PhoneBook
+phoneBook =      
+    [("betty","555-2938")     
+    ,("bonnie","452-2928")     
+    ,("patsy","493-2928")     
+    ,("lucille","205-2928")     
+    ,("wendy","939-8282")     
+    ,("penny","853-2492")     
+    ]  
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+inPhoneBook name pnumber pbook =(name,pnumber) `elem` pbook
