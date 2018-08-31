35dea36a489ffd10582945e7bf4db3570970f27e
mm <markus.mueller.1.g@googlemail.com>
2018-08-31 15:17:27 +0200

    added a Record synatax example with a  where and a let version to the deconstrunction example

--- RealWorldHaskell/src/Deconstruction.hs
+++ RealWorldHaskell/src/Deconstruction.hs
@@ -4,3 +4,27 @@ data Shape  = Circle Double
 describe :: Shape ->String
 describe (Circle radius)            = "Circle with radus" ++ (show radius)
 describe (Rectangle width length)   = "Rectangle with widht=" ++ (show width) ++ " and length=" ++ (show length)                          
+-- alternative Solution with record syntax which  adds functions radius widht and length automatically 
+-- unfortunately lenght collides with 
+data Shape2  = Circle2 {radius::Double} 
+            | Rectangle2 {width:: Double, length::Double}
+              deriving (Show)
+              
+c2::Shape2
+c2 = Circle2 3
+r2::Shape2
+r2=Rectangle2 2 3
+describe2 :: Shape2 ->String
+describe2 (Circle2 x)  = show (radius c) 
+    where c=Circle2 x
+describe2 (Rectangle2 x y)  =show ( width r) ++ show (Main.length r)  
+    where r=Rectangle2 x y
+
+describe3 :: Shape2 ->String
+describe3 (Circle2 x)  = 
+    let c = Circle2 x
+    in show (radius c) 
+describe3 (Rectangle2 x y)  =
+    let r=Rectangle2 x y 
+    in show ( width r) ++ show (Main.length r)  
+   
