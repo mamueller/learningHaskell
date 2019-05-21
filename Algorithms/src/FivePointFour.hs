e18c5b50ed2d3be3bdbde19f22f5b5dad14aa4d5
mm <markus.mueller.1.g@googlemail.com>
2019-05-21 13:13:44 +0200

    started implementation of ordered list sets

---
+++ Algorithms/src/FivePointFour.hs
@@ -0,0 +1,53 @@
+-- Given a set of points represented by their coordinates (x,y) in a 2-D space, 
+-- the distance of a point of coordinates (x,y) from the origin is defined as sqrt(x**2+y**2)
+-- Define an appropriate type in Haskell and the corresponding overloading operation <= 
+-- to handle these points in a priority queue! We always want the point closer to the 
+-- origin to be at the front of the queue.
+module FivePointFour
+    (   
+        Set(..)
+        ,emptySet
+        ,setEmpty
+        ,addSet
+        ,delSet
+        ,inSet
+        ,included
+        ,inter
+        ,union
+    )
+where
+import Point
+-- implement the queue        
+newtype Set a = Set {elementlist::[a]}
+    deriving (Show)
+
+instance (Eq a)=>Eq (Set a)
+    where 
+        s1 == s2 =and [ (included s1 s2) , (included s2 s1)]
+
+emptySet ::(Eq a)=> Set a
+emptySet =Set []
+
+setEmpty ::Set a -> Bool
+setEmpty (Set []) = True
+setEmpty _       = False
+
+addSet ::(Eq a)=>  a -> Set a -> Set a
+addSet x (Set q) = Set (x:q)
+
+delSet ::(Eq a)=> a -> Set a -> Set a 
+delSet _ (Set [] ) = error "delSet: cannot delelte from empty set"
+delSet x (Set xs) = Set (filter (/= x) xs) 
+
+inSet :: (Eq a)=> Set a -> a -> Bool
+inSet s p = or [ x==p |x <- (elementlist s )]   
+               
+included :: (Eq a)=> Set a -> Set a ->Bool
+included s1 s2 = and [ inSet s2 x| x <- elementlist s1]  
+
+inter :: (Eq a)=> Set a -> Set a -> Set a 
+inter s1 s2 = Set [ x| x <- elementlist s1,(inSet s2 x)]  
+
+union :: (Eq a)=> Set a -> Set a -> Set a 
+union s1 s2 = Set ( (elementlist s1) ++ (elementlist s2))
+
---
+++ Algorithms/test/TestFivePointFour.hs
@@ -0,0 +1,84 @@
+module Main where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import FivePointFour
+import Point
+
+p1 :: Point
+p1 = Point{x=1, y=1}
+p2 :: Point 
+p2 = Point {x=2, y=2}
+p3 :: Point 
+p3 = Point {x=3, y=2}
+
+myCompFunc :: Point->Point->Bool
+myCompFunc p1 p2 = (dist p1) <= (dist p2)
+
+main :: IO ()
+main = do
+    defaultMain (testGroup "set Tests" [
+         equalSetTest 
+        ,setEmptyTest_1
+        ,setEmptyTest_2
+        ,addSetTest
+        ,delSetTest
+        ,inSetTest
+        ,notInSetTest
+        ,includedTest
+        ,notIncludedTest
+        ,interTest
+        ,emptyInterTest
+        ,unionTest
+        ])
+
+
+equalSetTest:: TestTree
+equalSetTest= testCase "Testing equality of sets constructed from lists with duplicates "
+    (assertEqual "Should return True" True ( (Set [p1,p1,p2])==(Set [p1,p2])))
+
+setEmptyTest_1:: TestTree
+setEmptyTest_1= testCase "Testing empty set "
+    (assertEqual "Should return True" True ( setEmpty (Set [])))
+
+setEmptyTest_2:: TestTree
+setEmptyTest_2= testCase "Testing one element set "
+    (assertEqual "Should return False " False ( setEmpty (Set [p1] )))
+
+addSetTest::TestTree
+addSetTest = testCase "testing that an element is inserted "
+    (assertEqual "should return " (Set [p2,p1,p3]) (addSet p2 (Set [p1,p3] ))) 
+
+delSetTest::TestTree
+delSetTest = testCase "testing that an element is deleted "
+    (assertEqual "should return " [p2,p3] (elementlist (delSet p1 (Set [p1,p2,p3] )))) 
+
+inSetTest::TestTree
+inSetTest = testCase "testing that in set"
+    (assertEqual "should return True" True (inSet (Set [p1,p2]) p1))
+
+notInSetTest::TestTree
+notInSetTest = testCase "testing that in set"
+    (assertEqual "should return False" False (inSet (Set [p1,p2]) p3))
+
+includedTest::TestTree
+includedTest = testCase "testing that set a is in set b"
+    (assertEqual "should return True" True (included (Set [p1,p2]) (Set [p1,p2,p3])))
+
+notIncludedTest::TestTree
+notIncludedTest = testCase "testing that set a is not in set b"
+    (assertEqual "should return False" False (included (Set [p1,p2,p3]) (Set [p1,p2])))
+
+interTest::TestTree
+interTest = testCase "testing the intersection of two sets"
+    (assertEqual "should return " (Set [p1,p2]) (inter (Set [p1,p2]) (Set [p1,p2,p3])))
+
+emptyInterTest::TestTree
+emptyInterTest = testCase "testing the intersection of two disjunct sets"
+    (assertEqual "should return " (Set []) (inter (Set [p1,p2]) (Set [p3])))
+
+unionTest::TestTree
+unionTest = testCase "testing the intersection of two disjunct sets"
+    (assertEqual "should return " (Set [p1,p2,p3]) (union (Set [p1,p2]) (Set [p1,p3])))
+
