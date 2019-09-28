43037ad6cc74f331bfc1e479cbceba3d8226c427
Markus Müller <markus.mueller.1.g@googlemail.com>
2019-09-28 16:14:11 +0200

    added the random number generation to the code in main and added the dependency to the cabal file

--- FunctionalProgrammingCourseGlasgow/randomStarMan/app/Main.hs
+++ FunctionalProgrammingCourseGlasgow/randomStarMan/app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
+import System.Random
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+        ind <-randomRIO (1,length dict ) :: IO Int 
+        -- putStrLn (dict !! ind)
+        starman (dict !! ind) 5 
+
